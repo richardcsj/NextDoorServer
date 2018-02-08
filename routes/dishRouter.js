08b8e9956f180c80738b8775a26824c83eb24686
Richard Chun <richard.chun91@gmail.com>
2018-02-07 19:25:51 -0500

    comments populated with user

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -14,6 +14,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
 .get((req,res,next) => {
   Dishes.find({})
+  .populate('comments.author')
   .then((dishes) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'application/json');
@@ -49,6 +50,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
 .get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comments.author')
   .then((dish) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'application/json');
@@ -86,6 +88,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
 .get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comments.author')
   .then((dish) => {
     if (dish != null) {
       res.statusCode = 200;
@@ -104,6 +107,7 @@ dishRouter.route('/:dishId/comments')
   Dishes.findById(req.params.dishId)
   .then((dish) => {
     if (dish != null) {
+      req.body.author = req.user._id;
       dish.comments.push(req.body);
       dish.save()
       .then((dish) => {
@@ -151,6 +155,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req,res,next) => {
   Dishes.findById(req.params.dishId)
+  .populate('comments.author')
   .then((dish) => {
     if (dish != null && dish.comments.id(req.params.commentId) != null) {
       res.statusCode = 200;
